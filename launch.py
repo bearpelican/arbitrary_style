5f624032885004ca05519243fef27687d59f59e2
Andrew Shaw <andrew.t.shaw@gmail.com>
2018-11-03 14:11:58 -0700

    Creating traning folder and adding cluster launch.py

---
+++ launch.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+
+import argparse
+import ncluster
+import os
+
+IMAGE_NAME = 'pytorch.imagenet.source.v7'
+INSTANCE_TYPE = 'p3.2xlarge'
+NUM_GPUS = 8
+
+ncluster.set_backend('aws')
+parser = argparse.ArgumentParser()
+parser.add_argument('--name', type=str, default='style',
+                    help="name of the current run, used for machine naming and tensorboard visualization")
+parser.add_argument('--machines', type=int, default=1,
+                    help="how many machines to use")
+args = parser.parse_args()
+
+lr = 1.0
+bs = [512, 224, 128] # largest batch size that fits in memory for each image size
+bs_scale = [x/bs[0] for x in bs]
+one_machine = [
+  {'ep':(0,1),  'lr':(lr,lr*2)}, # lr warmup is better with --init-bn0
+  {'ep':(1,2), 'lr':(lr*2,lr/4)}, # trying one cycle
+]
+
+schedules = {1: one_machine}
+
+
+# routines to build NCCL ring orders
+def get_nccl_params(num_tasks, num_gpus):
+  if num_tasks <= 1:
+    return 'NCCL_DEBUG=VERSION'
+  nccl_rings = get_nccl_rings(num_tasks, num_gpus)
+  return f'NCCL_RINGS="{nccl_rings}" NCCL_SINGLE_RING_THRESHOLD=10 NCCL_DEBUG=VERSION'
+  # return 'NCCL_MIN_NRINGS=2 NCCL_SINGLE_RING_THRESHOLD=10 NCCL_DEBUG=VERSION'
+
+
+def get_nccl_rings(num_tasks, num_gpus):
+  ring = build_ring_order(range(num_tasks), range(num_gpus))
+  ring_rev = build_ring_order(reversed(range(num_tasks)),
+                              reversed(range(num_gpus)))
+  rotated_gpu_order = [3, 2, 1, 0, 7, 6, 5, 4]
+  skip_gpu_order = get_skip_order(num_gpus)
+  if (num_tasks >= 4) and (num_gpus == 8):
+    assert ((num_tasks % 4) == 0)
+    skip_machine_order = get_skip_order(num_tasks)
+    ring_skip = build_ring_order(skip_machine_order, rotated_gpu_order)
+    ring_skip_rev = build_ring_order(reversed(skip_machine_order),
+                                     skip_gpu_order)
+    rings_arr = [ring, ring_rev, ring_skip, ring_skip_rev]
+    # rings_arr = [ring, ring_rev, ring_skip]
+  else:
+    rings_arr = [ring, ring_rev]
+  return ' | '.join(rings_arr)
+
+
+def build_ring_order(machine_order, gpu_order):
+  gpu_order = list(gpu_order)
+  machine_order = list(machine_order)
+  ngpus = len(gpu_order)
+  r_order = [(x * ngpus) + y for x in machine_order for y in gpu_order]
+  return ' '.join(map(str, r_order))
+
+
+def get_skip_order(size):
+  if size == 4:
+    return [0, 2, 1, 3]
+  skip_step = 5 if size == 16 else 3
+  # step size of 3 yields - [0,3,6,1,4,7,2,5]
+  return [(i * skip_step) % size for i in range(size)]
+
+
+def format_params(arg):
+  if isinstance(arg, list) or isinstance(arg, dict):
+    return '\"' + str(arg) + '\"'
+  else:
+    return str(arg)
+
+
+def main():
+  supported_regions = ['us-west-2', 'us-east-1', 'us-east-2']
+  assert ncluster.get_region() in supported_regions, f"required AMI {IMAGE_NAME} has only been made available in regions {supported_regions}, but your current region is {ncluster.get_region()}"
+  assert args.machines in schedules, f"{args.machines} not supported, only support {schedules.keys()}"
+
+  os.environ['NCLUSTER_AWS_FAST_ROOTDISK'] = '1'  # use io2 disk on AWS
+  job = ncluster.make_job(name=args.name,
+                          run_name=f"{args.name}-{args.machines}",
+                          num_tasks=args.machines,
+                          image_name=IMAGE_NAME,
+                          instance_type=INSTANCE_TYPE,
+                          install_script=open('setup.sh').read(),
+                          spot=True
+                          )
+  job.upload('training')
+  job.run(f'source activate fastai')
+
+  nccl_params = get_nccl_params(args.machines, NUM_GPUS)
+
+  # Training script args
+  default_params = []
+
+  params = ['--phases', schedules[args.machines]]
+  training_params = default_params + params
+  training_params = ' '.join(map(format_params, training_params))
+
+  # TODO: simplify args processing, or give link to actual commands run
+  for i, task in enumerate(job.tasks):
+    dist_params = f'--nproc_per_node=8 --nnodes={args.machines} --node_rank={i} --master_addr={job.tasks[0].ip} --master_port={6006}'
+    cmd = f'{nccl_params} python -m torch.distributed.launch {dist_params} training/train.py {training_params}'
+    task.run(f'echo {cmd} > {job.logdir}/task-{i}.cmd')  # save command-line
+    task.run(cmd, non_blocking=True)
+
+  print(f"Logging to {job.logdir}")
+
+
+if __name__ == '__main__':
+  main()
