c8048abfc44a56b522e1aa9f1c22420b88fe1e57
Andrew Shaw <andrew.t.shaw@gmail.com>
2018-11-06 09:11:21 +0000

    Refactoring StyleTransferTrain notebook

--- training/data.py
+++ training/data.py
@@ -14,21 +14,26 @@ class ContentStyleLoader():
     def __init__(self, cont_dl, style_dl, repeat_xy=True):
         self.cont_dl,self.style_dl = cont_dl,style_dl
         self.repeat_xy=repeat_xy
+        self.count_c = 0
+        self.count_s = 0
         
     def __iter__(self):
         it_c = iter(self.cont_dl)
         it_s = iter(self.style_dl)
         
         for b_c in it_c:
-            try: b_s = next(it_s)
+            self.count_c += 1
+            
+            try: 
+                b_s = next(it_s)
+                self.count_s += 1
             except: 
                 print('Restarting style')
-                it_s = iter(style_dl)
-                b_s = next(it_s)
+                it_s = iter(self.style_dl)
+                
             out = b_c[0], b_s[0]
             if self.repeat_xy: yield out, out
             else: yield out
-            
         
     def __len__(self):
         return len(self.cont_dl)
---
+++ training/dist.py
@@ -0,0 +1,16 @@
+import torch.distributed.deprecated as dist
+from torch.utils.data.distributed import DistributedSampler
+from torch.nn.parallel.deprecated import DistributedDataParallel
+
+
+class DDP(DistributedDataParallel):
+    def load_state_dict(self, *args, **kwargs): self.module.load_state_dict(*args, **kwargs)
+    def state_dict(self, *args, **kwargs): return self.module.state_dict(*args, **kwargs)
+
+def reduce_tensor(tensor): return sum_tensor(tensor)/env_world_size()
+def sum_tensor(tensor):
+    rt = tensor.clone()
+    dist.all_reduce(rt, op=dist.reduce_op.SUM)
+    return rt
+def env_world_size(): return int(os.environ.get('WORLD_SIZE', 1))
+def env_rank(): return int(os.environ.get('RANK',0))
