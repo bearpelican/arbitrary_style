f69763d4615b093616623fdc2221c0c64d9173a9
Andrew Shaw <andrew.t.shaw@gmail.com>
2018-11-06 08:34:57 +0000

    Creating fastai notebook

---
+++ training/data.py
@@ -0,0 +1,40 @@
+from fastai import *
+from fastai.vision import *
+
+class InputDataset(Dataset):
+    def __init__(self, fns:FilePathList):
+        super().__init__()
+        self.x  = np.array(fns)
+        self.image_opener = open_image
+
+    def __getitem__(self, i): return self.image_opener(self.x[i]),0    
+    def __len__(self): return len(self.x)
+    
+class ContentStyleLoader():
+    def __init__(self, cont_dl, style_dl, repeat_xy=True):
+        self.cont_dl,self.style_dl = cont_dl,style_dl
+        self.repeat_xy=repeat_xy
+        
+    def __iter__(self):
+        it_c = iter(self.cont_dl)
+        it_s = iter(self.style_dl)
+        
+        for b_c in it_c:
+            try: b_s = next(it_s)
+            except: 
+                print('Restarting style')
+                it_s = iter(style_dl)
+                b_s = next(it_s)
+            out = b_c[0], b_s[0]
+            if self.repeat_xy: yield out, out
+            else: yield out
+            
+        
+    def __len__(self):
+        return len(self.cont_dl)
+    
+class SimpleDataBunch():
+    def __init__(self, train_dl, path): 
+        self.train_dl,self.path = train_dl,path
+        self.device,self.loss_func = defaults.device,None
+        self.valid_dl = None
---
+++ training/loss.py
@@ -0,0 +1,49 @@
+from torch import nn
+import torch
+from fastai import *
+
+class TransferLoss(nn.Module):
+    def __init__(self, m_vgg, cont_wgt, style_wgt, style_block_wgts, tva_wgt, data_norm, c_block=1, sum_loss=True):
+        super().__init__()
+        self.style_wgt,self.style_block_wgts = style_wgt,style_block_wgts
+        self.cont_wgt,self.tva_wgt = cont_wgt,tva_wgt
+        self.m_vgg,self.data_norm,self.c_block = m_vgg,data_norm,c_block
+        self.sum_loss = sum_loss
+        
+    def forward(self, input, x_cont, x_style):
+        style_wgts = [self.style_wgt*b for b in self.style_block_wgts]
+        bs = x_cont.size(0)
+        with torch.no_grad(): 
+            style_batch = x_style.repeat(bs,1,1,1)
+            s_out = self.m_vgg(style_batch)
+            style_feat = [s.clone() for s in s_out]
+            
+            cont_feat = self.m_vgg(x_cont)[self.c_block].clone()
+            
+        input_norm,_ = self.data_norm((input,None))
+        inp_feat = self.m_vgg(input_norm)
+        
+        closs = [ct_loss(inp_feat[self.c_block],cont_feat) * self.cont_wgt]
+        sloss = [gram_loss(inp,targ)*wgt for inp,targ,wgt in zip(inp_feat, style_feat, style_wgts) if wgt > 0]
+        tvaloss = tva_loss(input_norm) * self.tva_wgt
+        
+        if self.sum_loss: 
+            total_loss = closs + sloss + [tvaloss]
+            return sum(total_loss)
+        else: return closs, sloss, tvaloss
+        
+# Loss Functions
+# losses
+def ct_loss(input, target): return F.mse_loss(input,target)
+
+def gram(input):
+        b,c,h,w = input.size()
+        x = input.view(b, c, -1)
+        return torch.bmm(x, x.transpose(1,2))/(c*h*w)
+
+def gram_loss(input, target): return F.mse_loss(gram(input), gram(target))
+
+def tva_loss(y):
+    w_var = torch.sum(torch.abs(y[:, :, :, :-1] - y[:, :, :, 1:]))
+    h_var = torch.sum(torch.abs(y[:, :, :-1, :] - y[:, :, 1:, :]))
+    return w_var + h_var
